1eefa1a5a55f13812942cc57174f5de4c76dfc44
Shashank Kothari <marutikothari39@gmail.com>
2024-04-19 08:24:02 +0530

    Initial commit

---
+++ Home.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+st.set_page_config(
+    page_title="CSV Analysis APP",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+#importing dataset
+df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
+try:
+    dataset=st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+    df = pd.read_csv(dataset)
+    try:
+        df.drop(['Unnamed: 0'],axis=1,inplace=True)
+    except:
+        ""     
+    df.to_csv('tmpdataset.csv', index=False)
+    dsstr=dataset.name
+    del dataset
+except:
+    dsstr="Randomly generated"
+    df.to_csv('tmpdataset.csv', index=False)
+finally:
+    ""   
+
+dsstr+' Dataset:'
+st.dataframe(df.style.highlight_max(axis=0))
+
+st.write('Mean:')
+st.write(df.mean())
+st.write('\nMedian:')
+st.write(df.median())
+st.write('Mode:')
+st.write(df.mode(axis='columns', numeric_only=True))
+st.write('\nStandard Deviation:')
+st.write(df.std())
+st.write('\ncorrelation coefficient:')
+st.write(df.corr())
+st.write('\ncorvariance:')
+st.write(df.cov())
+
+st.line_chart(df)
+df.plot.scatter(x=list(df)[0], y=list(df)[1], title='Scatter Plot')
+st.scatter_chart(
+    df,
+    x=list(df)[0],
+    y=list(df)[1],
+)
+
+st.bar_chart(df)
+fig, ax = plt.subplots()
+ax.hist(df, bins=20)
+st.pyplot(fig)
+
---
+++ pages/chat.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import streamlit as st
+import json, csv
+from keys import myKey
+import google.generativeai as genai 
+
+import pathlib
+import textwrap
+from IPython.display import display
+from IPython.display import Markdown
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+
+
+genai.configure(api_key=myKey)
+model = genai.GenerativeModel('gemini-pro')
+
+st.set_page_config(
+    page_title="CSV Analysis APP",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+df = pd.read_csv('tmpdataset.csv')
+
+def csv_to_json(csv_file):
+    data = []
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return json.dumps(data, indent=4)
+
+try:
+    for message in chat.history:
+        display(to_markdown(f'**{message.role}**: {message.parts[0].text}'))
+except:
+    ""  
+
+chatForm = st.form('chat_form')
+query = chatForm.text_input('Enter your query:')
+submit = chatForm.form_submit_button(f'Search')
+  
+if submit:
+    dson=csv_to_json("tmpdataset.csv")
+    chat = model.start_chat(history=[])
+    response = chat.send_message(dson+" "+query)
+    st.write(response.text)
